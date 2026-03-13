14868149e630c1d57262857e1b321f9f3452a15b
samet2001 <samet44firinci@gmail.com>
2026-03-13 14:27:57 +0300

    Initial Commit

---
+++ pomodoro/pomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  pomodoroApp.swift
+//  pomodoro
+//
+//  Created by SAMET FIRINCI on 13.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct pomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
